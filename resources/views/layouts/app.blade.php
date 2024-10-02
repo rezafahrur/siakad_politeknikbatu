56aa7507fe1d50345fb3870e79eaa72b7af472f3
Rizaldy Ali Machfuddin <alirizaldy357@gmail.com>
2024-10-02 10:31:32 +0700

    update logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,7 @@
     <link rel="stylesheet" href="{{ asset('assets/css/demo1/style.css') }}">
     <!-- End layout styles -->
 
-    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" />
+    <link rel="shortcut icon" href="{{ asset('assets/images/logo/logo.png') }}" />
 </head>
 
 <body>
